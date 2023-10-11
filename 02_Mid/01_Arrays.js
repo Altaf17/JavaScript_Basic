4fdf2311dea22d2197e2a57c63b04e3f5c7464f0
Mohammed Altaf Hussain <101961251+Altaf17@users.noreply.github.com>
2023-10-11 08:10:10 +0000

    arrays continous

--- 02_Mid/01_Arrays.js
+++ 02_Mid/01_Arrays.js
@@ -23,9 +23,43 @@ const myArr = arr.join(); // converts to strings
 // console.log(myArr);
 
 const myNewArr = ["Altaf", "Adil", "Arham"];
-console.log("slice",myNewArr.slice(0, 1)); // slice method takes start and end index .its not return the end element.its return upto end value. and its not mouted orginal array
-console.log(" after slice", myNewArr);
+// console.log("slice", myNewArr.slice(0, 1)); // slice method takes start and end index .its not return the end element.its return upto end value. and its not mouted orginal array
+// console.log(" after slice", myNewArr);
 
+// console.log("splice", myNewArr.splice(0, 1)); // splice method takes start and end index .its also return the end element . and its  mouted orginal array
+// console.log("after splice", myNewArr);
 
-console.log("splice", myNewArr.splice(0, 1));// splice method takes start and end index .its also return the end element . and its  mouted orginal array
-console.log("after splice", myNewArr);
+// continous
+const Heros = ["thor", "Ironman", "spiderman"];
+const kidsFev = ["moto", "patlo", "doremon"];
+
+const concateArray = kidsFev.concat(Heros); // merge to array and its return new array
+
+// console.log(concateArray);
+
+const allHeros = [...Heros, ...kidsFev]; // merged with spread operator
+// console.log(allHeros);
+
+const arr2 = [1, 2, 3, 4, 5, [6, 7], [8, 9, [10]]];
+const flatArr = arr2.flat(Infinity);
+// console.log(flatArr);
+
+const xyz = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const fli = xyz.reduce((acc, curr) => {
+  return acc.concat(curr);
+});
+// console.log(fli);
+
+// console.log(Array.isArray("React"));
+// console.log(Array.from("React"));
+// console.log(Array.from({ name: "Altaf", city: "bidar" }));
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
