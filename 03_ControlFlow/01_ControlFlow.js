10d2f3da94fa47f3413decc535a18a58a4ee05f4
Mohammed Altaf Hussain <101961251+Altaf17@users.noreply.github.com>
2023-10-13 09:09:28 +0000

    control flow

---
+++ 03_ControlFlow/01_ControlFlow.js
@@ -0,0 +1,75 @@
+// conditional checks if , if else, ||, &&, > , < , >=,  =<,  ==, ===, !==,!,
+
+// const isLoggedIn = true;
+// if (isLoggedIn) {
+//   console.log("Inside if");
+//}
+
+// const temperature = 32;
+// if (temperature > 35) {
+//   console.log(`very hot day tem is: ${temperature}`);
+// } else {
+//   console.log(`warm weather day tem is: ${temperature}`);
+// }
+
+// const balance = 1000;
+
+// if (balance > 500) {
+//   const bookPrice = 500;
+//   console.log(`you can buy book of Rs ${bookPrice}. you have ${balance} rupees`);
+// }
+
+// ternary operator
+const isLoggedIn = true;
+// console.log(isLoggedIn ? "welcome" : "please login");
+
+// switch statement
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    break;
+}
+
+//falsy values
+// false,0,-0, BigInt 0n,"",null,undefined,NaN,
+
+// truthy value
+// true , "0", "false", " ", [],{},function (){},
